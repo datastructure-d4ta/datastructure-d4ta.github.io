e510e41899ebceed2b39c4814ce400554a5f07e8
mrturn-geo <costenwc@fcpsstudent.us>
2026-09-10 09:54:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 setTimeout(()=>{
-  document.getAttributeById("2").textContent="Do you like yellow?"
-  document.getAttributeById("3").textContent="What about green?"
+  document.getElementById("2").textContent="Do you like yellow?";
+  document.getElementById("3").textContent="What about green?";
 setTimeout(()=>{
-  document.getAttributeById("1").textContent="Do you also like white?"
-}, 2000)}, 2000)
+  document.getElementById("1").textContent="Do you also like white?";
+}, 2000);}, 2000);
 
