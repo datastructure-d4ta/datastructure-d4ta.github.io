aae405bf813f45c0e1d3ff62e7e73a6bdec8e12f
mrturn-geo <costenwc@fcpsstudent.us>
2026-09-11 08:34:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 setTimeout(()=>{
   document.getElementById("2").textContent="Do you like yellow?";
-  document.getElementById("3").textContent="What about green?";
+  document.getElementById("3").textContent="What about red?";
 setTimeout(()=>{
   document.getElementById("1").textContent="Do you also like white?";
-}, 2000);}, 2000);
+}, 4000);}, 1562);
 
